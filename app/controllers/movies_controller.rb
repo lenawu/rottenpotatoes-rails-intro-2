e7f915189297c6d7d69a4021cbb4e1105fe3922e
lenawu <lenawu@berkeley.edu>
2016-02-17 04:02:21 +0000

    movie.all

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,8 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index    
+  def index   
+    @movies = Movie.all 
     #handles if a sort is needed and keep track of session
     sort = params[:sort] || session[:sort]
     case sort
